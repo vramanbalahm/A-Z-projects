6a6e861499d8be9b56c4b65b0a04d0660722db5e
Vijey <vramanbala@gmail.com>
2022-04-16 18:08:56 +0530

    first commit

---
+++ Simple_linear_regression.py
@@ -0,0 +1,42 @@
+#to predict the value of a variable with a simple linear equation 
+# a simple meathod 
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+dataset = pd.read_csv('Salary_Data.csv')
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, -1].values
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 1/3, random_state = 0)
+
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor.fit(X_train,y_train)
+
+y_pred = regressor.predict(X_test)
+
+plt.scatter(X_train, y_train, color = 'red')
+plt.plot(X_train, regressor.predict(X_train), color = 'blue')
+plt.title('Salary vs Experience (Training set)')
+plt.xlabel('Years of Experience')
+plt.ylabel('Salary')
+plt.show()
+
+# visualizing the test set
+plt.scatter(X_test, y_test, color = 'red')
+plt.plot(X_train, regressor.predict(X_train), color = 'blue')
+plt.title('Salary vs Experience (Test set)')
+plt.xlabel('Years of Experience')
+plt.ylabel('Salary')
+plt.show()
+
+print(regressor.predict([[12]]))
+print(regressor.coef_)
+print(regressor.intercept_)
+
+#finally the value of the salary is 
+salary = regressor.coef_ * 12 + regressor.intercept_
+print(salary)
